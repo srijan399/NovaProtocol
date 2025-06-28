09e81bb48f29f9300871c635a53e0377f9d613eb
srijansarkar <srijansarkar2705@gmail.com>
2025-06-28 09:05:14 +0000

    changes

--- main/app/_providers/provider.tsx
+++ main/app/_providers/provider.tsx
@@ -1,32 +1,35 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider } from "connectkit";
 import { WagmiProvider } from "wagmi";
-import { config } from './wagmi';
-import { theme } from '@/constants/theme';
-import { useState } from 'react';
+import { config } from "./wagmi";
+import { theme } from "@/constants/theme";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    // Create QueryClient with useState to prevent re-initialization
-    const [queryClient] = useState(() => new QueryClient({
+  // Create QueryClient with useState to prevent re-initialization
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
         defaultOptions: {
-            queries: {
-                staleTime: 60 * 1000,
-            },
+          queries: {
+            staleTime: 60 * 1000,
+          },
         },
-    }));
+      })
+  );
 
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <ConnectKitProvider
-                    customTheme={theme}
-                    // mode='dark'
-                >
-                    {children}
-                </ConnectKitProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
-    );
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider
+          customTheme={theme}
+          // mode='dark'
+        >
+          {children}
+        </ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
